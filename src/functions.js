d92cb46ec57402fa7f146facff3e948edcbcc25d
Alisenaqubadi <131773898+Alisenaqubadi@users.noreply.github.com>
2025-08-01 17:44:50 +0500

    Home page is completed

---
+++ src/functions.js
@@ -0,0 +1,25 @@
+function Create(element, container, className="", elementId=""){
+    const Element_value = document.createElement(element);
+    const Container_value = document.querySelector(container);
+    if (className !== ""){Element_value.className = className;}
+     if (elementId !== ""){Element_value.id = elementId;}
+    Container_value.appendChild(Element_value);
+}
+
+function Write(container, text) {
+    document.querySelectorAll(container).forEach(el => {
+        el.innerText = text;
+    });
+}
+
+function Writebyid(container, text) {
+    document.querySelectorAll(container).forEach(el => {
+        el.innerText = text;
+    });
+}
+
+
+export {
+    Create,
+    Write,
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
 import "./style.css"
+import { Create, Write } from "./functions.js"
+
+Create("div", "#content", "", "Home")
+Create("h1", "#Home", "", "restaurant-name")
+Write("#restaurant-name", "Honey Restaurant")
+Create("p", "#Home", "", "text")
+Write("#text", "Honey Restaurant is a warm, inviting place known for its cozy atmosphere and delicious homemade meals. Inspired by comfort and tradition, it offers a mix of hearty classics and sweet treats made with love—just like home. From fresh salads and grilled meats to its signature honey-glazed desserts, every dish is crafted with care. Whether you're here for a quick lunch or a relaxing dinner, Honey Restaurant makes you feel welcome, satisfied, and a little bit spoiled.")
+Create("div", "#Home", "", "time-table")
+Create("ul", "#time-table", "", "time")
+Create("li", "#time", "working-days");
+Create("li", "#time", "working-days");
+Create("li", "#time", "working-days");
+Create("li", "#time", "working-days");
+Create("li", "#time", "working-days");
+Create("li", "#time", "working-days");
+Write(".working-days", "from 9:00 a.m. to 11:00 p.m.")
+Create("div", "#Home", "", "location")
+Create("p", "#location", "", "location1")
+Write("#location1", "Address: 123 Honey Lane, Sweetville, ST 45678, USA")
+
+window.remove = function(what1="", what2=""){
+    document.getElementById(what1)?.remove();
+    if(what2!=="")
+    document.getElementById(what2)?.remove();
+}
